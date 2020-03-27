ee5a12f3567b804de4f32114b013e2c2cd87342c
Rodrigo V Verissimo <rodrigovenancioverissimo@gmail.com>
2020-03-27 08:55:21 -0400

    Finalizado frontend web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
+
+/**
+ * JSX ( JavaScript + XML )
+ * Componentes = Conjunto de funções
+ * O React não usa variáveis convensionais
+ * ele usa Estado para refletir as atualizações
+ * de na aplicação
+ *
+ * useState -> Array [ valor, funcaoDeAtualizacao ]
+ * no React há o principio da imutabilidade
+ *
+ * componentes devem ficar dentro de algo no react
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
